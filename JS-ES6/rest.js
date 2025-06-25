b04601d0692e9076801f70f28c0f2e7a7c926b80
poojitha-86 <tpoojitha335@gmail.com>
2025-06-25 15:18:15 +0530

    Rest-Opertor

---
+++ JS-ES6/rest.js
@@ -0,0 +1,12 @@
+function getInfo(a,b,c,d){
+    console.log(a,b,c,d);
+    return "pooji";
+}
+console.log(1,2,3);
+
+function getInfo2(a,b,...xyz){
+    console.log(a,b);
+    console.log(xyz);
+    return a,b;
+}
+console.log(1,2,3,4,5,6,7,8,9,10);
