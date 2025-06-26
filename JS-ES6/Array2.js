d6eb28a6dc90fc997cd690cf5affd3246f196303
poojitha-86 <tpoojitha335@gmail.com>
2025-06-26 20:14:58 +0530

    Array2.html

---
+++ JS-ES6/Array2.js
@@ -0,0 +1,24 @@
+const names = [
+    "pooji",
+    "jishi",
+    "Indhu",
+    "Bhavi"
+];
+
+let getArrayFirstValue = names[0];
+let getArraySecondValue = names[1];
+console.log(getArrayFirstValue,getArraySecondValue);
+
+const [
+    arrayFirstElement,
+    arraySecondElement,
+    arrayThirdElement,
+    arrayFourthElemt,
+] = numbers;
+
+console.log(
+    arrayFirstElement,
+    arraySecondElement,
+    arrayThirdElement,
+    arrayFourthElemt,
+);
